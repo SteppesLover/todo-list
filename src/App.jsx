981fdf6d33911204f4873a3bd7fa011cb20723be
SteppesLover <116759576+SteppesLover@users.noreply.github.com>
2025-07-24 16:01:22 -0700

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -4,10 +4,10 @@ import TodoForm from './features/TodoForm.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [filteredTodos, setTodoList] = useState([]); 
+  const [todos, setTodoList] = useState([]); 
   
 function updateTodo(editedTodo) {
-  const updatedTodos = filteredTodos.map((todo) => {
+  const updatedTodos = todos.map((todo) => {
     if (todo.id === editedTodo.id) {
       return { ...editedTodo };
     }
@@ -18,7 +18,7 @@ function updateTodo(editedTodo) {
 }
 
   function completeTodo(id){
-  const updatedTodos = filteredTodos.map((todo) => {
+  const updatedTodos = todos.map((todo) => {
   if(todo.id===id){
     return {...todo, isCompleted: true}
   }
@@ -30,14 +30,14 @@ function updateTodo(editedTodo) {
 
   function addTodo(title) {
     const newTodo = { title, id: Date.now(), isCompleted: false };
-    setTodoList([...filteredTodos, newTodo]);
+    setTodoList([...todos, newTodo]);
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={filteredTodos} onCompleteTodo={completeTodo}   onUpdateTodo={updateTodo} />
+      <TodoList todoList={todos} onCompleteTodo={completeTodo}   onUpdateTodo={updateTodo} />
     </div>
   );
 }

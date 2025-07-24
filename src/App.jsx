d95163afc83899e418afa074b69f6c558647e198
SteppesLover <116759576+SteppesLover@users.noreply.github.com>
2025-07-24 15:33:41 -0700

    Revert "Lesson 06 assignment"

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,21 @@
 import './App.css';
-import TodoList from './features/TodoList/TodoList.jsx';
-import TodoForm from './features/TodoForm.jsx';
+import TodoList from './TodoList.jsx';
+import TodoForm from './TodoForm.jsx';
 import { useState } from 'react';
 
 function App() {
-  const [filteredTodos, setTodoList] = useState([]); 
-  
-function updateTodo(editedTodo) {
-  const updatedTodos = filteredTodos.map((todo) => {
-    if (todo.id === editedTodo.id) {
-      return { ...editedTodo };
-    }
-    return todo;
-  });
-
-  setTodoList(updatedTodos);
-}
-
-  function completeTodo(id){
-  const updatedTodos = filteredTodos.map((todo) => {
-  if(todo.id===id){
-    return {...todo, isCompleted: true}
-  }
-  return todo;  
-  });
-
-  setTodoList(updatedTodos);
-  }
+  const [todoList, setTodoList] = useState([]); 
 
   function addTodo(title) {
-    const newTodo = { title, id: Date.now(), isCompleted: false };
-    setTodoList([...filteredTodos, newTodo]);
+    const newTodo = { title, id: Date.now() };
+    setTodoList([...todoList, newTodo]);
   }
 
   return (
     <div>
       <h1>Todo List</h1>
       <TodoForm onAddTodo={addTodo} />
-      <TodoList todoList={filteredTodos} onCompleteTodo={completeTodo}   onUpdateTodo={updateTodo} />
+      <TodoList todoList={todoList} />
     </div>
   );
 }
